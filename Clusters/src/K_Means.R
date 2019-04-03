d79c2cfa56c2908cbc11b6f6a41f13f892596f63
Alex Barrales <alexfabianb94@gmail.com>
2019-04-03 15:08:35 -0300

    fix: fix graphics K_Means.R (#2)

--- Clusters/src/K_Means.R
+++ Clusters/src/K_Means.R
@@ -19,9 +19,24 @@ summary(grade)
 
 wss=numeric(15)
 for (i in 1:15) {
-  data_out_kmeans <- kmeans(grade,centers=i,nstart = 25)
   wss[i] <- data_out_kmeans$tot.withinss
 }
 
+data_out_kmeans <- kmeans(grade,centers=3,nstart = 25)
 plot(1:15, wss, "b")
 
+df <- grade
+
+df$Cluster=factor(data_out_kmeans$cluster)
+Centers=as.data.frame(data_out_kmeans$centers)
+
+# Graphics ----------------------------------------------------------------------------------------------------------------------------
+
+g1=ggplot(data=df, aes(x=English, y=Math, color=Cluster))+geom_point()+theme(legend.position = "right")+geom_point(data=Centers, aes(x=English, y=Math, color=as.factor(c(1,2,3))), size=10, alpha=.3, show.legend =FALSE)
+g2=ggplot(data=df, aes(x=English, y=Science, color=Cluster))+geom_point()+theme(legend.position = "right")+geom_point(data=Centers, aes(x=English, y=Science, color=as.factor(c(1,2,3))), size=10, alpha=.3, show.legend =FALSE)
+g3=ggplot(data=df, aes(x=Math, y=Science, color=Cluster))+geom_point()+theme(legend.position = "right")+geom_point(data=Centers, aes(x=Math, y= Science, color=as.factor(c(1,2,3))), size=10, alpha=.3, show.legend =FALSE)
+tmp=ggplot_gtable(ggplot_build(g1))
+
+#grid.arrange(arrangeGrob(g1+theme(legend.position ="none"), g2+theme(legend.position ="none"), g3+theme(legend.position ="none"),main="High School Students Cluster Analysys",ncol=1))
+
+grid.arrange(g1, g2, g3,top="High School Students Cluster Analysys",ncol=3)
